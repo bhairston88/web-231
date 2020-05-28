9c4a7b24baf0d1a4cdd60b8124b3bb1dcc97490a
Brooklyn Hairston <bhairston@my365.bellevue.edu>
2020-05-28 16:26:19 -0400

    adding first commit

---
+++ week-1/hairston-primitive-types.js
@@ -0,0 +1,33 @@
+/*
+========================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 28 May 2020
+; Modified by: Brooklyn Hairston
+; Description: Primitive Types
+;========================================
+*/
+
+// variable declaration 
+var firstName;
+var lastName;
+var age;
+var street;
+var city;
+var zipCode;
+
+//variable assignment
+firstName = "Brooklyn";
+lastName = "Hairston";
+age = 31;
+street = "4567 Beautiful Ln";
+city = "Cleveland";
+zipCode = 12345;
+
+//output
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
