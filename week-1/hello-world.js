a87ca445c2490f5bd92c823ecda1b8193688ba1b
Brooklyn Hairston <bhairston@my365.bellevue.edu>
2020-05-28 16:02:27 -0400

    staged file

---
+++ week-1/hello-world.js
@@ -0,0 +1,11 @@
+/*
+========================================
+; Title: Excerise 1.3
+; Author: Professor Krasso
+; Date: 28 May 2020
+; Modified by: Brooklyn Hairston
+; Description: Demonstration of how to 
+; run JavaScript application in VS Code 
+;========================================
+*/
+console.log("Hello World, my name is Brooklyn Hairston");
