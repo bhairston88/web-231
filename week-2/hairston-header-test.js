38381c9e863b277acf292b0270771697a6856a9f
Brooklyn Hairston <bhairston@my365.bellevue.edu>
2020-06-03 20:58:43 -0400

    updating repository

---
+++ week-2/hairston-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  hairston-header-test.js
+; Author: Brooklyn Hairston
+; Date:   3 June 2020
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../hairston-header.js')
+
+console.log(header.display('Brooklyn','Hairston','Header'))
