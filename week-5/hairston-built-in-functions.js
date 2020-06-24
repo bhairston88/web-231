d84dfbd0688137b6ade2e975df0b6f6850393afe
Brooklyn Hairston <bhairston@my365.bellevue.edu>
2020-06-24 13:44:17 -0400

    adding exercise 5.2

---
+++ week-5/hairston-built-in-functions.js
@@ -0,0 +1,30 @@
+/*
+============================================
+; Title:  hairston-built-in-functions.js
+; Author: Brooklyn Hairston
+; Date:   23 June 2020
+; Modified by: Brooklyn Hairston
+; Description: Demonstrates how to loop through an array
+; using the forEach function
+;===========================================
+*/
+
+//header
+const header = require('./hairston-header.js')
+
+console.log(header.display('Brooklyn','Hairston', 'Built-in Functions'))
+
+//start program
+
+//array declaration
+
+var favFoods = ["potatoes", "cheese", "pasta", "fruit", "burritos"];
+
+//new line
+console.log("\n");
+
+//output
+
+favFoods.forEach(function(food) {
+  console.log(food)
+});
